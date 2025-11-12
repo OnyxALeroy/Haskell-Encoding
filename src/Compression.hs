654e9563140d42aca519fa1b32e1f65676d69c54
rom100main <rom100main@proton.me>
2025-11-12 18:17:12 +0100

    feat: compress base

---
+++ src/Compression.hs
@@ -0,0 +1,20 @@
+module Compression where
+
+import Huffman (HuffmanTree(..), huffmanEncode, buildHuffmanTree)
+import Data.List (group, sort)
+
+compress :: String -> FilePath -> String
+compress input path =
+    let
+        freqList = map (\xs -> (head xs, length xs)) . group . sort $ input
+        leaves = [Leaf c f | (c, f) <- freqList]
+        tree = buildHuffmanTree leaves
+        codes = huffmanEncode tree
+        lookupCode c = case lookup c codes of
+            Just code -> code
+            Nothing -> error "Character not found in Huffman codes"
+        encoded = concatMap lookupCode input
+    in encoded
+
+decompress :: String -> FilePath -> String
+decompress input path = "Decompression not implemented yet"
