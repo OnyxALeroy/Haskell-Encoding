0c283adeb2cc71a10878b4bb5383dfb50719e466
Antoine Dupret <antoinedupret6@gmail.com>
2025-11-12 17:57:46 +0100

    [FEAT] Matrix making & sorting

---
+++ src/BurrowsWheeler.hs
@@ -0,0 +1,27 @@
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let s = shifts "ABEACADABEA"
+  putStrLn "Original: ABEACADABEA"
+  putStrLn "Unsorted:"
+  mapM_ print s
+  putStrLn "\nSorted:"
+  mapM_ print (sortShifts s)
+
+shiftRight :: [a] -> [a]
+shiftRight [] = []
+shiftRight xs =
+  case reverse xs of
+    []     -> []
+    (y:ys) -> y : reverse ys
+
+shifts :: [a] -> [[a]]
+shifts xs = take (length xs) (iterate shiftRight xs)
+
+sortShifts :: Ord a => [[a]] -> [[a]]
+sortShifts xss = sort xss
+
+indexInSorted :: (Ord a, Eq a) => [a] -> [[a]] -> Int
+indexInSorted xs sorted =
+  fromMaybe (-1) (elemIndex xs sorted)
