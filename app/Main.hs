99d4c6da9b833e3d62828fe46faafba8a8fc3bf2
Antoine Dupret <antoinedupret6@gmail.com>
2025-11-14 11:41:13 +0100

    [FEAT] Adding bwt to Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Compression (compress, decompress)
+import BurrowsWheeler (bwTransform)
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -12,12 +13,19 @@ main = do
             let result = compress content
             writeFile output result
             putStrLn result
+
         ["decompress", input, output] -> do
             content <- readFile input
             let result = decompress content
             writeFile output result
             putStrLn result
+                    
+        ["bwt", input] -> do
+            let (idx, lastCol) = bwTransform input
+            putStrLn (show idx ++ " " ++ lastCol)
+
         _ -> do
             putStrLn "Usage:"
             putStrLn "  haskell-encoding compress <input> <output>"
             putStrLn "  haskell-encoding decompress <input> <output>"
+            putStrLn "  haskell-encoding bwt <string>"
--- src/BurrowsWheeler.hs
+++ src/BurrowsWheeler.hs
@@ -1,15 +1,7 @@
 module BurrowsWheeler where
 
-import Data.List (sort)
-
-main :: IO ()
-main = do
-  let s = shifts "ABEACADABEA"
-  putStrLn "Original: ABEACADABEA"
-  putStrLn "Unsorted:"
-  mapM_ print s
-  putStrLn "\nSorted:"
-  mapM_ print (sortShifts s)
+import Data.List (sort, elemIndex)
+import Data.Maybe (fromMaybe)
 
 shiftRight :: [a] -> [a]
 shiftRight [] = []
@@ -22,8 +14,16 @@ shifts :: [a] -> [[a]]
 shifts xs = take (length xs) (iterate shiftRight xs)
 
 sortShifts :: Ord a => [[a]] -> [[a]]
-sortShifts xss = sort xss
+sortShifts = sort
 
 indexInSorted :: (Ord a, Eq a) => [a] -> [[a]] -> Int
 indexInSorted xs sorted =
   fromMaybe (-1) (elemIndex xs sorted)
+
+bwTransform :: Ord a => [a] -> (Int, [a])
+bwTransform xs =
+  let m      = shifts xs
+      sorted = sortShifts m
+      idx    = indexInSorted xs sorted
+      lasts  = map last sorted
+  in  (idx + 1, lasts)
