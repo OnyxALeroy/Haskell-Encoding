8378e595fb123cceab4acd1399c1b62fa9f8d627
rom100main <rom100main@proton.me>
2025-11-14 14:34:15 +0100

    feat: escape and unescape char, parse symbols in Huffman table, decompress

--- src/Compression.hs
+++ src/Compression.hs
@@ -1,6 +1,6 @@
 module Compression where
 
-import Huffman (HuffmanTree(..), huffmanEncode, buildHuffmanTree)
+import Huffman (HuffmanTree(..), huffmanEncode, huffmanDecode, buildHuffmanTree)
 import Data.List (group, sort)
 
 compress :: String -> String
@@ -14,9 +14,46 @@ compress input =
             Just code -> code
             Nothing -> error "Character not found in Huffman codes"
         encoded = concatMap lookupCode input
-        symbolsTable = concatMap (\(c, code) -> [c] ++ ":" ++ code ++ ",") codes
+        symbolsTable = concatMap (\(c, code) -> escapeChar c ++ ":" ++ code ++ ",") codes
         result = "SYMBOLS: " ++ init symbolsTable ++ "\nDATA: " ++ encoded
     in result
 
 decompress :: String -> String
-decompress input = "Decompression not implemented yet"
+decompress input =
+    let
+        symbolsLine = lines input !! 0
+        symbolsStr = drop (length "SYMBOLS: ") symbolsLine
+        dataLine = lines input !! 1
+        dataStr = drop (length "DATA: ") dataLine
+        symbolPairs = parseSymbols symbolsStr
+        codes = [(c, code) | (code, c) <- symbolPairs]
+        decoded = huffmanDecode codes dataStr
+    in decoded
+
+parseSymbols :: String -> [(String, Char)]
+parseSymbols "" = []
+parseSymbols str =
+    let (symbolPair, rest) = parseSymbolPair str
+    in symbolPair : parseSymbols (drop 1 rest)
+
+parseSymbolPair :: String -> ((String, Char), String)
+parseSymbolPair str =
+    let (escapedChar, afterColon) = break (== ':') str
+        char = unescapeChar escapedChar
+        (code, afterComma) = break (== ',') (tail afterColon)
+    in ((code, char), afterComma)
+
+unescapeChar :: String -> Char
+unescapeChar "\\n" = '\n'
+unescapeChar "\\r" = '\r'
+unescapeChar "\\t" = '\t'
+unescapeChar "\\\\" = '\\'
+unescapeChar [c] = c
+unescapeChar _ = error "Invalid escape sequence"
+
+escapeChar :: Char -> String
+escapeChar '\n' = "\\n"
+escapeChar '\r' = "\\r"
+escapeChar '\t' = "\\t"
+escapeChar '\\' = "\\\\"
+escapeChar c = [c]
